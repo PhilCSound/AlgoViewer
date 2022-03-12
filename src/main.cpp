1563fe7539a21793dc218a6c434ea67cab2240b7
Phillip Cook <yru2kind@gmail.com>
2022-03-12 16:41:50 -0600

    Made initial state AlgoDisplayState

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,13 @@
 #pragma once
 #include "Application.hpp"
+#include "AlgoDisplayState.hpp"
 
 int main()
 {   
 
     Application app;
-
+    State* initState = new AlgoDisplayState();
+    app.PushState(initState);
     while (app.IsRunning())
     {
         app.Update();
