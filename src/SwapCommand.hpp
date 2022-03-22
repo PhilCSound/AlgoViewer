06783992e653007b7886e6897929c7d66ac2e74c
Phillip Cook <yru2kind@gmail.com>
2022-03-21 23:39:27 -0500

    Added a swap command

---
+++ src/SwapCommand.hpp
@@ -0,0 +1,19 @@
+#pragma once
+#include "Command.hpp"
+
+class SwapCommand : public Command
+{
+public:
+    SwapCommand(int indexLeft, int indexRight) 
+        { m_indexL = indexLeft; m_indexR = indexRight; }
+    
+    void execute(DataDeck& data) override
+    {
+        data.swap(m_indexL, m_indexR);
+    }
+    
+    private:
+    int m_indexL = 0;
+    int m_indexR = 0;
+
+};
