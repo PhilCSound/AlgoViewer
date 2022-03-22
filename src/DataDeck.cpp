ef6c41dffa73b6eee9b03cf55aae104c70099155
Phillip Cook <yru2kind@gmail.com>
2022-03-21 23:44:03 -0500

    Works for now but will be working on refactoring

--- src/DataDeck.cpp
+++ src/DataDeck.cpp
@@ -51,6 +51,15 @@ void DataDeck::lookAtIndex(int index, bool indexI)
     }
 }
 
+void DataDeck::swap(int indexL, int indexR)
+{
+    m_animState = EnumAnimationState::SWAPPING;
+    m_elapsedTime = sf::Time::Zero;
+    m_indexToSwapL = indexL;
+    m_indexToSwapR = indexR;
+}
+
+
 const bool DataDeck::isAnimating() const
 {
     return (m_animState != EnumAnimationState::NOT_ANIM);
@@ -59,6 +68,8 @@ const bool DataDeck::isAnimating() const
 void DataDeck::update(sf::Time dt)
 {
     m_elapsedTime += dt;
+    float theta = m_elapsedTime.asMilliseconds() / SWAP_ANIM_IN_MS;
+    theta = theta;
     switch(m_animState)
     {
         case EnumAnimationState::LOOKING:
@@ -67,6 +78,20 @@ void DataDeck::update(sf::Time dt)
                 StopAnim();
                 break;
             }
+            break;
+        case EnumAnimationState::SWAPPING:
+
+            if (theta >= 1.0f)
+            {
+                theta = 1.0f;
+                AnimQuadSwap(m_indexToSwapL, m_indexToSwapR, theta);
+                SwapIndexes(m_indexToSwapL, m_indexToSwapR);
+                StopAnim();
+                return;
+            }
+            else
+                AnimQuadSwap(m_indexToSwapL, m_indexToSwapR, theta);
+            return;
         default:
             break;
     }
@@ -76,20 +101,24 @@ void DataDeck::CreateVertexArray()
 {
     int i = 0;
     int pixMax = MAX_NUM_DATA * PIX_SIZE;
+
     for (short& data : m_shortData)
     {
-        sf::Vertex* quad = &m_vertexArray[i * 4];
-        quad[0].position = sf::Vector2f(i * PIX_SIZE, pixMax);
-        quad[1].position = sf::Vector2f(i * PIX_SIZE, pixMax - PIX_SIZE * data);
-        quad[2].position = sf::Vector2f((i + 1) * PIX_SIZE, pixMax - PIX_SIZE * data);
-        quad[3].position = sf::Vector2f((i + 1)* PIX_SIZE, pixMax);
-        //Color
-        quad[0].color = DEF_COLOR_1;
-        quad[1].color = DEF_COLOR_2;
-        quad[2].color = DEF_COLOR_2;
-        quad[3].color = DEF_COLOR_1;
+        sf::Vector2f botLeftPos = sf::Vector2f(i * PIX_SIZE, pixMax);
+        ChangeQuadPos(i, botLeftPos, data);
+        ChangeQuadColor(i, DEF_COLOR_1, DEF_COLOR_2);
         i++;
     }
+}
+
+void DataDeck::ChangeQuadPos(int index, sf::Vector2f botLeftPos, short data)
+{
+    sf::Vertex* quad = &m_vertexArray[index * 4];
+    float dataHeight = PIX_SIZE * data;
+    quad[0].position = botLeftPos;
+    quad[1].position = sf::Vector2f(botLeftPos.x, botLeftPos.y - dataHeight);
+    quad[2].position = sf::Vector2f(botLeftPos.x + PIX_SIZE, botLeftPos.y - dataHeight);
+    quad[3].position = sf::Vector2f(botLeftPos.x + PIX_SIZE, botLeftPos.y);
 }
 
 void DataDeck::ChangeQuadColor(int index, sf::Color color1, sf::Color color2)
@@ -105,4 +134,27 @@ void DataDeck::StopAnim()
 {
     m_animState = EnumAnimationState::NOT_ANIM;
     m_elapsedTime = sf::Time::Zero;
+}
+
+//Theta is a value between 0.0f and 1.0f that indicates when fully swapped
+void DataDeck::AnimQuadSwap(int index1, int index2, float theta)
+{
+    int pixMax = MAX_NUM_DATA * PIX_SIZE;
+    //Quad1's default botLeft position (at theta = 0.0f) followed by Quad 2
+    sf::Vector2f quad1InitPos = sf::Vector2f(index1 * PIX_SIZE, pixMax);
+    sf::Vector2f quad2InitPos = sf::Vector2f(index2 * PIX_SIZE, pixMax);
+
+    sf::Vector2f quadOffset = (quad2InitPos - quad2InitPos) * theta;
+    ChangeQuadPos(index1, quad1InitPos + quadOffset, m_shortData[index1]);
+    ChangeQuadPos(index2, quad2InitPos - quadOffset, m_shortData[index2]);
+}
+
+void DataDeck::SwapIndexes(int index1, int index2)
+{
+    int pixMax = MAX_NUM_DATA * PIX_SIZE;
+    std::swap(m_shortData.at(index1), m_shortData.at(index2));
+    sf::Vector2f quad1InitPos = sf::Vector2f(index1 * PIX_SIZE, pixMax);
+    sf::Vector2f quad2InitPos = sf::Vector2f(index2 * PIX_SIZE, pixMax);
+    ChangeQuadPos(index1, quad1InitPos, m_shortData.at(index1));
+    ChangeQuadPos(index2, quad2InitPos, m_shortData.at(index2));
 }
--- src/DataDeck.hpp
+++ src/DataDeck.hpp
@@ -15,6 +15,7 @@ public:
     void shuffleData();
     void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
     void lookAtIndex(int index, bool indexI = true);
+    void swap(int indexL, int indexR);
     const bool isAnimating() const;
     void update(sf::Time dt);
 
@@ -22,6 +23,7 @@ private:
     const unsigned int MAX_NUM_DATA = 100;
     const float PIX_SIZE = 8.0f;
     const float LOOK_ANIM_IN_MS = 3.0f;
+    const float SWAP_ANIM_IN_MS = 6.0f;
     const sf::Color DEF_COLOR_1 = sf::Color(102, 126, 234);
     const sf::Color DEF_COLOR_2 = sf::Color(118, 75, 162);
     const sf::Color INDEX_I_COLOR_1 = sf::Color(19, 84, 122);
@@ -34,10 +36,15 @@ private:
     std::random_device m_randomDevice;
     int m_indexI = 0;
     int m_indexJ = 0;
+    int m_indexToSwapL = 0;
+    int m_indexToSwapR = 0;
     sf::Time m_elapsedTime;
     EnumAnimationState m_animState = EnumAnimationState::NOT_ANIM;
 
     void CreateVertexArray();
+    void ChangeQuadPos(int index, sf::Vector2f botLeftPos, short data);
     void ChangeQuadColor(int index, sf::Color color1, sf::Color color2);
+    void AnimQuadSwap(int index1, int index2, float theta);
     void StopAnim();
+    void SwapIndexes(int index1, int index2);
 };
