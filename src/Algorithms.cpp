8f9f5a84a002944eb449eca8c6acc172ccef9dc4
Phillip Cook <yru2kind@gmail.com>
2022-03-21 23:42:56 -0500

    Fixed formating and added swap cmd selection sort

--- src/Algorithms.cpp
+++ src/Algorithms.cpp
@@ -2,34 +2,22 @@
 
 namespace Algo
 {
-
-std::queue<Command*> SelectionSort(std::vector<short> data)
-{
-    std::queue<Command*> q;
-    int smallestIndex = 0;
-    for (int i = 0; i < data.size() - 1; i++)
+    std::queue<Command *> SelectionSort(std::vector<short> data)
     {
-        q.push(new LookCommand(i));
-        for (int j = i + 1; j < data.size(); j++)
+        std::queue<Command *> q;
+        int smallestIndex = 0;
+        for (int i = 0; i < data.size() - 1; i++)
         {
-            q.push(new LookCommand(j, false));
-            if (data[j] < data[smallestIndex])
-                smallestIndex = j;
+            q.push(new LookCommand(i));
+            for (int j = i + 1; j < data.size(); j++)
+            {
+                q.push(new LookCommand(j, false));
+                if (data[j] < data[smallestIndex])
+                    smallestIndex = j;
+            }
+            std::swap(data.at(i), data.at(smallestIndex));
+            q.push(new SwapCommand(i, smallestIndex));
         }
-        std::swap(data.at(i), data.at(smallestIndex));
+        return q;
     }
-    return q;
-}
-
-
-
-
-
-
-
-
-
-
-
-
 }
