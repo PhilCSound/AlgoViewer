0cded8abfa993c0a31dd035f177aa58e03a8416b
Phillip Cook <yru2kind@gmail.com>
2022-04-01 02:31:44 -0500

    Changed from queue to deque

--- src/Algorithms.cpp
+++ src/Algorithms.cpp
@@ -2,22 +2,22 @@
 
 namespace Algo
 {
-    std::queue<Command *> SelectionSort(std::vector<short> data)
+    std::deque<Command *> SelectionSort(std::vector<short> data)
     {
-        std::queue<Command *> q;
+        std::deque<Command *> q;
         int smallestIndex = 0;
         for (int i = 0; i < data.size() - 1; i++)
         {
-            smallestIndex = data[i];
-            q.push(new LookCommand(i));
+            smallestIndex = i;
+            q.push_back(new LookCommand(i));
             for (int j = i + 1; j < data.size(); j++)
             {
-                q.push(new LookCommand(j, false));
+                q.push_back(new LookCommand(j, false));
                 if (data[j] < data[smallestIndex])
                     smallestIndex = j;
             }
             std::swap(data.at(i), data.at(smallestIndex));
-            q.push(new SwapCommand(i, smallestIndex));
+            q.push_back(new SwapCommand(i, smallestIndex));
         }
 
         return q;
